3702c9f58224847db14d78e72692aac4f32f03da
Scott Nortman <scott.nortman@gmail.com>
2020-01-02 09:18:27 -0500

    Added shell script commands for D435; Tried rosparam called from lunch file

--- scripts/realsense_stereo_setup.sh
+++ scripts/realsense_stereo_setup.sh
@@ -3,16 +3,34 @@
 # Taken from
 # https://answers.ros.org/question/51474/can-i-run-a-bash-script-using-roslaunch/?answer=157051?answer=157051#post-id-157051
 
+# This works for the D415 camera
+# rosrun dynamic_reconfigure dynparam set /camera/stereo_module \
+# "{'exposure': 5000,
+# 'gain' : 100,
+# 'visual_preset' : 0,
+# 'frames_queue_size' : 8,
+# 'laser_power' : 0.0,
+# 'inter_cam_sync_mode' : 0.0,
+# 'enable_auto_exposure' : 0,
+# 'enable_auto_white_balance' : 1, #
+# 'error_polling_enabled' : 1,
+# 'output_trigger_enabled' : 0,
+# 'global_time_enabled' : 1
+# }"
+
+#D435
 rosrun dynamic_reconfigure dynparam set /camera/stereo_module \
 "{'exposure': 500,
-'gain' : 10,
+'gain' : 100,
 'visual_preset' : 0,
 'frames_queue_size' : 8,
 'laser_power' : 0.0,
 'inter_cam_sync_mode' : 0.0,
 'enable_auto_exposure' : 0,
-'enable_auto_white_balance' : 1,
 'error_polling_enabled' : 1,
 'output_trigger_enabled' : 0,
 'global_time_enabled' : 1
 }"
+
+
+
